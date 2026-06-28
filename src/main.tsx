6cc1d9ce1229aff23dcd123b93276c30d9d4f506
lfwf <155892906+lfwf@users.noreply.github.com>
2026-06-28 22:42:26 +0800

    Import training phase styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './advisor.css';
 import './layout-overrides.css';
+import './training-phase.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
